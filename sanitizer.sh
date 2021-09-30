e948ffc099b4d60cd99ef3838b47f237d0d32cce
Qiang Liu <cyruscyliu@gmail.com>
2021-09-30 14:27:58 +0200

    add sanitizer.sh

---
+++ sanitizer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+VERSION=$1 # usually 5 or 6
+cd ../qemu/build-san-$VERSION
+if [ $VERSION == '6' ]; then
+    CC=clang CXX=clang++ ../configure --enable-fuzzing --enable-debug \
+        --disable-werror --enable-sanitizers \
+        --target-list="i386-softmmu arm-softmmu aarch64-softmmu"
+    ninja qemu-fuzz-i386 qemu-fuzz-arm qemu-fuzz-aarch64
+    cd $OLDPWD
+elif [ $VERSION == '5' ]; then
+    CC=clang CXX=clang++ ../configure --enable-debug \
+        --disable-werror --enable-sanitizers \
+        --target-list="i386-softmmu arm-softmmu aarch64-softmmu"
+    make CONFIG_FUZZ=y CFLAGS="-fsanitize=fuzzer" -j$(nproc) \
+        i386-softmmu/fuzz arm-softmmu/fuzz aarch64-softmmu/fuzz
+    cp i386-softmmu/qemu-fuzz-i386 .
+    cp arm-softmmu/qemu-fuzz-arm .
+    cp aarch64-softmmu/qemu-fuzz-aarch64 .
+    cd $OLDPWD
+fi
