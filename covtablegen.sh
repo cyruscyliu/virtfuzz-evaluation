a7754001535ed75486ea0d654e9cf90742916b0e
Qiang Liu <cyruscyliu@gmail.com>
2021-10-02 11:31:21 +0200

    try to fix timestamp order

--- covtablegen.sh
+++ covtablegen.sh
@@ -1,3 +1,3 @@
 FILE=$1
 PATTERN=$2
-grep $FILE $PATTERN* | xargs -i echo {} | tr -s ' ' | awk -F":|-| " '{print $6,$4$5,$20}' | python3 covtablegen.py
+grep $FILE $PATTERN* | sort -t- -k6 | xargs -i echo {} | tr -s ' ' | awk -F":|-| " '{print $6,$4$5,$20}' | python3 covtablegen.py
