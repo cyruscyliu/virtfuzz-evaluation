8d2259181c0a3795b9050cddc29ccecda59eaace
Qiang Liu <cyruscyliu@gmail.com>
2021-10-11 13:08:01 +0200

    update ohci results

--- replot.sh
+++ replot.sh
@@ -26,5 +26,5 @@ python3 cov24plot.py virtfuzz-ehci.csv virtfuzz-m-ehci.csv virtfuzz-f-ehci.csv q
 python3 cov24plot.py qtest-cs4231a.csv
 python3 cov24plot.py qtest-rtl8139.csv
 python3 cov24plot.py virtfuzz-ehci.csv qtest-ehci.csv
-python3 cov24plot.py virtfuzz-ohci.csv virtfuzz-m-ohci.csv qtest-ohci.csv
+python3 cov24plot.py virtfuzz-ohci.csv virtfuzz-m-ohci.csv virtfuzz-f-ohci.csv qtest-ohci.csv
 python3 cov24plot.py qtest-megasas.csv
