34532a0b6ef73e13cf3d878af9a84cdfabca2f89
Qiang Liu <cyruscyliu@gmail.com>
2021-10-12 08:42:58 +0200

    update cs4231a

--- replot.sh
+++ replot.sh
@@ -23,7 +23,7 @@ python3 cov24plot.py virtfuzz-e1000.csv qtest-e1000.csv
 python3 cov24plot.py virtfuzz-uhci.csv qtest-uhci.csv
 python3 cov24plot.py virtfuzz-uhci.csv virtfuzz-m-uhci.csv virtfuzz-f-uhci.csv qtest-uhci.csv
 python3 cov24plot.py virtfuzz-ehci.csv virtfuzz-m-ehci.csv virtfuzz-f-ehci.csv qtest-ehci.csv
-python3 cov24plot.py qtest-cs4231a.csv
+python3 cov24plot.py virtfuzz-cs4231a.csv qtest-cs4231a.csv
 python3 cov24plot.py virtfuzz-rtl8139.csv qtest-rtl8139.csv
 python3 cov24plot.py virtfuzz-ehci.csv qtest-ehci.csv
 python3 cov24plot.py virtfuzz-ohci.csv virtfuzz-m-ohci.csv virtfuzz-f-ohci.csv qtest-ohci.csv
