ef8a9ed06ba872a92930594f4cde7200b6cafae1
Qiang Liu <cyruscyliu@gmail.com>
2021-10-19 15:22:26 +0200

    update a wraper

---
+++ bugger.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -x
+
+TARGET=$1
+export UBSAN_OPTIONS=symbolize=1:halt_on_error=0:print_stacktrace=1
+QEMU_BIN=$PWD/../qemu/build-clean-less-crashes-5/qemu-fuzz-i386
+for ROUND in $(seq 0 9); do
+    LLVM_PROFILE_FILE=profile-virtfuzz-$TARGET-$ROUND cpulimit -l 100 -- $QEMU_BIN --fuzz-target=stateful-fuzz-$TARGET -max_total_time=1200 >virtfuzz-$TARGET-$ROUND.log 2>&1 &
+done
