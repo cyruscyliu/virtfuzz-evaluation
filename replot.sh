1427482bb7cc9425546a8d4dfb4129eeedbcdf37
Qiang Liu <cyruscyliu@gmail.com>
2022-01-13 17:51:01 +0100

    update data

--- replot.sh
+++ replot.sh
@@ -1,23 +1,26 @@
 # evaluation 1
-python3 cov24plot.py nyx-ac97.csv qtest-ac97.csv virtfuzz-ac97.csv
-python3 cov24plot.py qtest-ac97.csv virtfuzz-ac97.csv
-python3 cov24plot.py nyx-cs4231a.csv qtest-cs4231a.csv virtfuzz-cs4231a.csv
-python3 cov24plot.py qtest-cs4231a.csv virtfuzz-cs4231a.csv
-python3 cov24plot.py qtest-es1370.csv virtfuzz-es1370.csv
-python3 cov24plot.py qtest-ahci.csv virtfuzz-ahci.csv
-python3 cov24plot.py qtest-sdhci.csv virtfuzz-sdhci.csv
-python3 cov24plot.py qtest-megasas.csv virtfuzz-megasas.csv
-python3 cov24plot.py qtest-e1000.csv virtfuzz-e1000.csv
-python3 cov24plot.py qtest-ne2000.csv virtfuzz-ne2000.csv
-python3 cov24plot.py qtest-pcnet.csv virtfuzz-pcnet.csv
-python3 cov24plot.py qtest-rtl8139.csv virtfuzz-rtl8139.csv
+python3 cov24plot.py nyx-ac97.csv virtfuzz-ac97.csv qtest-ac97.csv
+python3 cov24plot.py virtfuzz-ac97.csv qtest-ac97.csv
+python3 cov24plot.py nyx-cs4231a.csv virtfuzz-cs4231a.csv qtest-cs4231a.csv
+python3 cov24plot.py virtfuzz-cs4231a.csv qtest-cs4231a.csv
+python3 cov24plot.py virtfuzz-es1370.csv qtest-es1370.csv
+python3 cov24plot.py virtfuzz-ahci.csv qtest-ahci.csv
+python3 cov24plot.py virtfuzz-sdhci.csv qtest-sdhci.csv
+python3 cov24plot.py virtfuzz-megasas.csv qtest-megasas.csv
+python3 cov24plot.py virtfuzz-e1000.csv qtest-e1000.csv
+python3 cov24plot.py virtfuzz-ne2000.csv qtest-ne2000.csv
+python3 cov24plot.py virtfuzz-pcnet.csv qtest-pcnet.csv
+python3 cov24plot.py virtfuzz-rtl8139.csv qtest-rtl8139.csv
 python3 cov24plot.py virtfuzz-ati.csv virtfuzz-ati2d.csv
 python3 cov24plot.py virtfuzz-cirrus-vga.csv # miss qtest-cirrus-vga.csv
-python3 cov24plot.py qtest-uhci.csv virtfuzz-uhci.csv
-python3 cov24plot.py qtest-ohci.csv virtfuzz-ohci.csv
-python3 cov24plot.py qtest-ehci.csv virtfuzz-ehci.csv
+python3 cov24plot.py virtfuzz-uhci.csv qtest-uhci.csv
+python3 cov24plot.py virtfuzz-ohci.csv qtest-ohci.csv
+python3 cov24plot.py virtfuzz-ehci.csv qtest-ehci.csv
 
 # evaluation 2
 python3 cov24plot.py virtfuzz-uhci.csv virtfuzz-m-uhci.csv virtfuzz-f-uhci.csv qtest-uhci.csv
 python3 cov24plot.py virtfuzz-ehci.csv virtfuzz-m-ehci.csv virtfuzz-f-ehci.csv qtest-ehci.csv
 python3 cov24plot.py virtfuzz-ohci.csv virtfuzz-m-ohci.csv virtfuzz-f-ohci.csv qtest-ohci.csv
+python3 cov24plot.py virtfuzz-uhci.csv virtfuzz-m-uhci.csv virtfuzz-f-uhci.csv vshuttle-uhci.csv qtest-uhci.csv
+python3 cov24plot.py virtfuzz-ehci.csv virtfuzz-m-ehci.csv virtfuzz-f-ehci.csv vshuttle-ehci.csv qtest-ehci.csv
+python3 cov24plot.py virtfuzz-ohci.csv virtfuzz-m-ohci.csv virtfuzz-f-ohci.csv vshuttle-ohci.csv qtest-ohci.csv
