e7705e8de9b7286757f06993e25039b583ba5ffa
Qiang Liu <cyruscyliu@gmail.com>
2022-01-13 09:54:33 +0100

    update coverage overtime

--- replot.sh
+++ replot.sh
@@ -1,11 +1,23 @@
-python3 cov24plot.py virtfuzz-e1000.csv qtest-e1000.csv
-python3 cov24plot.py virtfuzz-uhci.csv qtest-uhci.csv
+# evaluation 1
+python3 cov24plot.py nyx-ac97.csv qtest-ac97.csv virtfuzz-ac97.csv
+python3 cov24plot.py qtest-ac97.csv virtfuzz-ac97.csv
+python3 cov24plot.py nyx-cs4231a.csv qtest-cs4231a.csv virtfuzz-cs4231a.csv
+python3 cov24plot.py qtest-cs4231a.csv virtfuzz-cs4231a.csv
+python3 cov24plot.py qtest-es1370.csv virtfuzz-es1370.csv
+python3 cov24plot.py qtest-ahci.csv virtfuzz-ahci.csv
+python3 cov24plot.py qtest-sdhci.csv virtfuzz-sdhci.csv
+python3 cov24plot.py qtest-megasas.csv virtfuzz-megasas.csv
+python3 cov24plot.py qtest-e1000.csv virtfuzz-e1000.csv
+python3 cov24plot.py qtest-ne2000.csv virtfuzz-ne2000.csv
+python3 cov24plot.py qtest-pcnet.csv virtfuzz-pcnet.csv
+python3 cov24plot.py qtest-rtl8139.csv virtfuzz-rtl8139.csv
+python3 cov24plot.py virtfuzz-ati.csv virtfuzz-ati2d.csv
+python3 cov24plot.py virtfuzz-cirrus-vga.csv # miss qtest-cirrus-vga.csv
+python3 cov24plot.py qtest-uhci.csv virtfuzz-uhci.csv
+python3 cov24plot.py qtest-ohci.csv virtfuzz-ohci.csv
+python3 cov24plot.py qtest-ehci.csv virtfuzz-ehci.csv
+
+# evaluation 2
 python3 cov24plot.py virtfuzz-uhci.csv virtfuzz-m-uhci.csv virtfuzz-f-uhci.csv qtest-uhci.csv
 python3 cov24plot.py virtfuzz-ehci.csv virtfuzz-m-ehci.csv virtfuzz-f-ehci.csv qtest-ehci.csv
-python3 cov24plot.py virtfuzz-cs4231a.csv qtest-cs4231a.csv
-python3 cov24plot.py virtfuzz-rtl8139.csv qtest-rtl8139.csv
-python3 cov24plot.py virtfuzz-ehci.csv qtest-ehci.csv
-python3 cov24plot.py virtfuzz-ohci.csv qtest-ohci.csv
 python3 cov24plot.py virtfuzz-ohci.csv virtfuzz-m-ohci.csv virtfuzz-f-ohci.csv qtest-ohci.csv
-python3 cov24plot.py virtfuzz-megasas.csv qtest-megasas.csv
-python3 cov24plot.py virtfuzz-ati.csv virtfuzz-ati2d.csv
