970b19f0d58ef779c521b7ca49834db1570fe58a
Qiang Liu <cyruscyliu@gmail.com>
2022-01-14 13:10:06 +0100

    update vshuttle

--- quick-bugs.sh
+++ quick-bugs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-target=$1
+arch=$1
+target=$2
 # stage 1
 pushd ../qemu/build-san-5
 make CONFIG_FUZZ=y CFLAGS="-fsanitize=fuzzer -g" -j$(nproc) \
@@ -9,6 +10,6 @@ cp i386-softmmu/qemu-fuzz-i386 .
 cp arm-softmmu/qemu-fuzz-arm .
 cp aarch64-softmmu/qemu-fuzz-aarch64 .
 # stage 2
-./qemu-fuzz-i386 --fuzz-target=videzzo-fuzz-${target} -detect_leaks=0
+./qemu-fuzz-${arch} --fuzz-target=videzzo-fuzz-${target} -detect_leaks=0
 # stage 3
 popd
--- replot.sh
+++ replot.sh
@@ -4,10 +4,13 @@ python3 cov24plot.py virtfuzz-ac97.csv qtest-ac97.csv
 python3 cov24plot.py nyx-cs4231a.csv virtfuzz-cs4231a.csv qtest-cs4231a.csv
 python3 cov24plot.py virtfuzz-cs4231a.csv qtest-cs4231a.csv
 python3 cov24plot.py virtfuzz-es1370.csv qtest-es1370.csv
+python3 cov24plot.py virtfuzz-intel-hda.csv qtest-intel-hda.csv
+python3 cov24plot.py virtfuzz-sb16.csv qtest-sb16.csv
 python3 cov24plot.py virtfuzz-ahci.csv qtest-ahci.csv
 python3 cov24plot.py virtfuzz-sdhci.csv qtest-sdhci.csv
 python3 cov24plot.py virtfuzz-megasas.csv qtest-megasas.csv
 python3 cov24plot.py virtfuzz-e1000.csv qtest-e1000.csv
+python3 cov24plot.py virtfuzz-eepro100.csv qtest-eepro100.csv
 python3 cov24plot.py virtfuzz-ne2000.csv qtest-ne2000.csv
 python3 cov24plot.py virtfuzz-pcnet.csv qtest-pcnet.csv
 python3 cov24plot.py virtfuzz-rtl8139.csv qtest-rtl8139.csv
