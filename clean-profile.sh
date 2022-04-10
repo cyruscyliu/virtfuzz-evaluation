edd6a582ec787e31e7309b52d8671c8085623404
Qiang Liu <cyruscyliu@gmail.com>
2022-04-10 11:10:05 +0800

    update

---
+++ clean-profile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# this script deletes coverage raw data that is not used any more.
+
+dir=$1
+prefix=$2
+
+mkdir /tmp/$dir
+for ROUND in $(seq 0 9); do
+    mv $(ls $prefix-$ROUND-* | tail -n 1) /tmp/$dir
+    rm $prefix-$ROUND-*
+done
+mv /tmp/$dir/* $dir/
