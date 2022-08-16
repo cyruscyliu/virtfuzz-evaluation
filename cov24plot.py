900ed5a46464a427d2171ff439954f0d18c53df0
Qiang Liu <cyruscyliu@gmail.com>
2022-08-16 19:37:00 +0800

    update cov24plot.py

--- cov24plot.py
+++ cov24plot.py
@@ -6,15 +6,16 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 plt.rcParams.update({'font.size': 24})
 
-def plot(metadata, linestyle, shadowcolor):
+def plot(metadata, linestyle, marker, color, shadowcolor):
     filename = metadata['filename']
     data = pd.read_csv(filename)
     data['timestamp'] -= data['timestamp'][0]
     label = metadata['fuzzer'].replace('videzzo', 'ViDeZZo').replace(
         'qemufuzzer', 'QEMUFuzzer').replace('vshuttle', 'V-Shuttle-S').replace(
         '-intel', '').replace('-cirrus', '').replace('nyx', 'NYX')
-    label += '-'
-    label += metadata['variant']
+    if metadata['variant'] != 'none':
+        label += '-'
+        label += metadata['variant']
     if metadata['target'] == 'ati':
         label += ' (ati)'
     elif metadata['target'] == 'ati2d':
@@ -24,9 +25,13 @@ def plot(metadata, linestyle, shadowcolor):
     elif metadata['target'] == 'e1000e_core':
         label += ' (e1000e_core)'
 
-    plt.plot(data['timestamp'], data['avg'], linestyle, linewidth=2, color='black', label=label)
+    plt.plot(data['timestamp'], data['avg'],
+             linestyle, linewidth=1,
+             color=color, label=label,
+             marker=marker, markersize=3)
     plt.fill_between(data['timestamp'], data['min'], data['max'], color=shadowcolor, alpha=0.8)
-    plt.legend(loc='upper left', fontsize='x-small')
+    plt.legend(loc='center left', fontsize=8,
+               bbox_to_anchor=(1.04, 0.5), fancybox=True, ncol=1)
 
 # name convention
 # videzzo-qemu-ehci-arp.csv
@@ -49,21 +54,37 @@ ax.set_aspect(4.9, adjustable='box')
 
 # shadowcolor = ['0.55', '0.65', '0.75', '0.85', '0.95']
 # shadowcolor = ['lightcoral', 'navajowhite', 'lightgreen', 'skyblue', 'plum']
-colorset = sys.argv[-1]
+
+videzzo_markers = ['o', 'v', '1', '8']
+qemufuzzer_markers = ['p']
+vshuttle_markers = ['s']
+nyx_markder = ['x']
+
+indicators = sys.argv[-1]
 shadowcolor = []
-for indicator in colorset:
+markers = []
+colors = []
+for idx, indicator in enumerate(indicators):
     if indicator == 'V':
+        colors.append('red')
         shadowcolor.append('pink')
+        markers.append(videzzo_markers[idx % len(videzzo_markers)])
     elif indicator == 'Q':
-        shadowcolor.append('orange')
+        colors.append('orange')
+        shadowcolor.append('peachpuff')
+        markers.append(qemufuzzer_markers[idx % len(qemufuzzer_markers)])
     elif indicator == 'N':
+        colors.append('blue')
         shadowcolor.append('cyan')
+        markers.append(nyx_markder[idx % len(nyx_markder)])
     elif indicator == 'S':
-        shadowcolor.append('plum')
+        colors.append('green')
+        shadowcolor.append('lightgreen')
+        markers.append(vshuttle_markers[idx % len(vshuttle_markers)])
 if len(shadowcolor) == 0:
     raise "unknow colorset {}".format(colorset)
 for i, md in enumerate(metadata):
-    plot(md, '-', shadowcolor[i])
+    plot(md, '-', markers[i], colors[i], shadowcolor[i])
 
 plt.xticks([10, 60, 600, 3600, 86400], ['10s', '1m', '10m', '1h', '24h'])
 plt.axvline(x=10, color='purple', linestyle='dotted')
