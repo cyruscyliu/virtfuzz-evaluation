76a03115b9353ab6d7ff0b54d94a4ae6e0821532
Qiang Liu <cyruscyliu@gmail.com>
2022-09-27 13:49:21 +0800

    update

--- results/replot-ev1.sh
+++ results/replot-ev1.sh
@@ -1,28 +1,28 @@
 # evaluation 1
 python3 ../cov24plot.py videzzo-qemu-ac97-arp.csv qtest-qemu-ac97-none.csv nyx-qemu-legacy_ac97-none.csv VQN
-python3 ../cov24plot.py videzzo-qemu-cs4231a-arp.csv qtest-qemu-cs4231a-none.csv VQ
-python3 ../cov24plot.py videzzo-qemu-es1370-arp.csv qtest-qemu-es1370-none.csv VQ
+python3 ../cov24plot.py videzzo-qemu-cs4231a-arp.csv qtest-qemu-cs4231a-none.csv nyx-qemu-legacy_cs4231a-none.csv VQN
+python3 ../cov24plot.py videzzo-qemu-es1370-arp.csv qtest-qemu-es1370-none.csv nyx-qemu-legacy_es1370-none.csv VQN
 python3 ../cov24plot.py videzzo-qemu-intelhda-arp.csv qtest-qemu-intelhda-none.csv nyx-qemu-legacy_intelhda-none.csv VQN
 python3 ../cov24plot.py videzzo-qemu-sb16-arp.csv qtest-qemu-sb16-none.csv nyx-qemu-legacy_sb16-none.csv VQN
 
 python3 ../cov24plot.py videzzo-qemu-ahci-arp.csv qtest-qemu-ahci-none.csv VQ
-python3 ../cov24plot.py videzzo-qemu-floppy-arp.csv qtest-qemu-floppy-none.csv VQ
-python3 ../cov24plot.py videzzo-qemu-sdhci-arp.csv qtest-qemu-sdhci-none.csv VQ
+python3 ../cov24plot.py videzzo-qemu-floppy-arp.csv qtest-qemu-floppy-none.csv nyx-qemu-legacy_floppy-none.csv VQN
+python3 ../cov24plot.py videzzo-qemu-sdhci-arp.csv qtest-qemu-sdhci-none.csv nyx-qemu-legacy_sdhci-none.csv VQN
 python3 ../cov24plot.py videzzo-qemu-megasas-arp.csv qtest-qemu-megasas-none.csv VQ
-python3 ../cov24plot.py videzzo-qemu-e1000-arp.csv qtest-qemu-e1000-none.csv VQ
+python3 ../cov24plot.py videzzo-qemu-e1000-arp.csv qtest-qemu-e1000-none.csv nyx-qemu-legacy_e1000-none.csv VQN
 
 python3 ../cov24plot.py videzzo-qemu-e1000e-arp.csv qtest-qemu-e1000e-none.csv VQ
 python3 ../cov24plot.py videzzo-qemu-e1000e_core-arp.csv qtest-qemu-e1000e_core-none.csv VQ
-python3 ../cov24plot.py videzzo-qemu-eepro100-arp.csv qtest-qemu-eepro100-none.csv VQ
-python3 ../cov24plot.py videzzo-qemu-ne2000-arp.csv qtest-qemu-ne2000-none.csv VQ
-python3 ../cov24plot.py videzzo-qemu-pcnet-arp.csv qtest-qemu-pcnet-none.csv VQ
-python3 ../cov24plot.py videzzo-qemu-rtl8139-arp.csv qtest-qemu-rtl8139-none.csv VQ
+python3 ../cov24plot.py videzzo-qemu-eepro100-arp.csv qtest-qemu-eepro100-none.csv nyx-qemu-legacy_ee100pro-none.csv VQN
+python3 ../cov24plot.py videzzo-qemu-ne2000-arp.csv qtest-qemu-ne2000-none.csv nyx-qemu-legacy_ne2000-none.csv VQN
+python3 ../cov24plot.py videzzo-qemu-pcnet-arp.csv qtest-qemu-pcnet-none.csv nyx-qemu-legacy_pcnet-none.csv VQN
+python3 ../cov24plot.py videzzo-qemu-rtl8139-arp.csv qtest-qemu-rtl8139-none.csv nyx-qemu-legacy_rtl8139-none.csv VQN
 
 python3 ../cov24plot.py videzzo-qemu-ati-arp.csv V
 python3 ../cov24plot.py videzzo-qemu-ati2d-arp.csv V
 python3 ../cov24plot.py videzzo-qemu-cirrusvga-arp.csv qtest-qemu-cirrusvga-none.csv VQ
 
+#  python3 ../cov24plot.py videzzo-qemu-ehci-arp.csv qtest-qemu-ehci-none.csv vshuttle-qemu-ehci-none.csv VQS
 python3 ../cov24plot.py videzzo-qemu-ohci-arp.csv qtest-qemu-ohci-none.csv vshuttle-qemu-ohci-none.csv VQS
-# python3 ../cov24plot.py videzzo-qemu-uhci.csv qtest-qemu-uhci.csv VQ
-# python3 ../cov24plot.py videzzo-qemu-uhci.csv qtest-qemu-uhci.csv vshuttle-uhci.csv VQV
-# python3 ../cov24plot.py videzzo-qemu-ehci.csv qtest-qemu-ehci.csv vshuttle-ehci.csv VQV
+#  python3 ../cov24plot.py videzzo-qemu-uhci-arp.csv qtest-qemu-uhci-none.csv vshuttle-qemu-uhci-none.csv VQS
+python3 ../cov24plot.py videzzo-qemu-xhci-arp.csv qtest-qemu-xhci-none.csv nyx-qemu-legacy_xhci-none.csv  VQN
---
+++ results/replot-ev2.sh
@@ -0,0 +1,9 @@
+python3 ../cov24plot.py \
+    videzzo-qemu-ohci-arp.csv videzzo-qemu-ohci-ap.csv  videzzo-qemu-ohci-rp.csv  videzzo-qemu-ohci-p.csv \
+    qtest-qemu-ohci-none.csv \
+    VVVVQ
+python3 ../cov24plot.py \
+    videzzo-qemu-xhci-arp.csv videzzo-qemu-xhci-ap.csv  videzzo-qemu-xhci-rp.csv  videzzo-qemu-xhci-p.csv \
+    qtest-qemu-xhci-none.csv \
+    nyx-qemu-legacy_xhci-none.csv nyx-qemu-qemu_xhci-none.csv \
+    VVVVQ
