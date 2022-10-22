c8d9f845686d493e3d0050957814a3b3f15566e8
Qiang Liu <cyruscyliu@gmail.com>
2022-10-22 15:36:21 +0800

    update

--- results/replot-ev2.sh
+++ results/replot-ev2.sh
@@ -6,17 +6,10 @@ python3 ../cov24plot.py \
 python3 ../cov24plot.py \
     videzzo-qemu-xhci-arp.csv videzzo-qemu-xhci-ap.csv  videzzo-qemu-xhci-rp.csv  videzzo-qemu-xhci-p.csv \
     qtest-qemu-xhci-none.csv \
-<<<<<<< HEAD
     nyx-qemu-legacy_xhci-none.csv \
-    VVVVQNN
-python3 ../cov24plot.py \
-    videzzo-qemu-xhci-arp.csv nyx-qemu-qemu_xhci-none.csv \
-    VN
-=======
-    VVVVQ
+    VVVVQN
 python3 ../cov24plot.py \
     videzzo-qemu-xhci-arp.csv \
     videzzo++-qemu-xhci-arp.csv \
     nyx-qemu-qemu_xhci-none.csv \
     VVN
->>>>>>> 526cdb015fd828de54c21c169e28bcf222a821fd
