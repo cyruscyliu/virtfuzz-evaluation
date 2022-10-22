4e896fc07366cf4fe6dc4b479337a1db54a3a679
Qiang Liu <cyruscyliu@gmail.com>
2022-10-22 19:51:55 +0800

    update marker size and color

--- results/replot-ev2.sh
+++ results/replot-ev2.sh
@@ -1,14 +1,14 @@
-python3 ../cov24plot.py \
+python3 ../cov24plot3.py \
     videzzo-qemu-ohci-arp.csv videzzo-qemu-ohci-ap.csv  videzzo-qemu-ohci-rp.csv  videzzo-qemu-ohci-p.csv \
     qtest-qemu-ohci-none.csv \
     vshuttle-qemu-ohci-none.csv  \
     VVVVSQ
-python3 ../cov24plot.py \
+python3 ../cov24plot3.py \
     videzzo-qemu-xhci-arp.csv videzzo-qemu-xhci-ap.csv  videzzo-qemu-xhci-rp.csv  videzzo-qemu-xhci-p.csv \
     qtest-qemu-xhci-none.csv \
     nyx-qemu-legacy_xhci-none.csv \
     VVVVQN
-python3 ../cov24plot.py \
+python3 ../cov24plot3.py \
     videzzo-qemu-xhci-arp.csv \
     videzzo++-qemu-xhci-arp.csv \
     nyx-qemu-qemu_xhci-none.csv \
---
+++ state-results/replot-ev3.sh
@@ -0,0 +1,7 @@
+# evaluation 3
+python3 ../cov24plot2.py \
+    videzzo-qemu-xhci-arp.states \
+    videzzo-qemu-xhci-arp.transitions \
+    videzzo++-qemu-xhci-arp.states \
+    videzzo++-qemu-xhci-arp.transitions \
+    VVPP
