0fcea18e0d5feecbd1a057850e895b80513a37f0
Qiang Liu <cyruscyliu@gmail.com>
2022-10-23 04:50:05 +0000

    update

--- statecovmerge.c
+++ statecovmerge.c
@@ -1,15 +1,18 @@
 #include "statecov.h"
 
 int main(int argc, char **argv) {
-    if (argc != 3) {
-        printf("usage: %s sprofile0 sprofile1\n", argv[0]);
+    if (argc < 3 || argc > 4) {
+        printf("usage: %s output sprofile0 [sprofile1]\n", argv[0]);
         exit(1);
     }
 
+    char pathname_output[256] = {'\0'};
     char pathname_state0[256] = {'\0'};
     char pathname_state1[256] = {'\0'};
-    memcpy(pathname_state0, argv[1], strlen(argv[1]));
-    memcpy(pathname_state1, argv[2], strlen(argv[2]));
+    memcpy(pathname_output, argv[1], strlen(argv[1]));
+    memcpy(pathname_state0, argv[2], strlen(argv[2]));
+    if (argc == 4)
+        memcpy(pathname_state1, argv[3], strlen(argv[3]));
 
     size_t Size = StateMachineSize * sizeof(StateMachine);
     uint8_t *Data0 = (uint8_t *)calloc(Size, 1);
@@ -23,22 +26,27 @@ int main(int argc, char **argv) {
     size_t ret0 = fread(Data0, 1, Size, f0);
     assert(ret0 == Size);
     fclose(f0);
-    FILE *f1 = fopen(pathname_state0, "rb");
-    if (f1 == NULL) {
-        printf("[-] %s failed to open. Exit.\n", pathname_state1);
-        exit(1);
+    if (argc == 4) {
+        FILE *f1 = fopen(pathname_state1, "rb");
+        if (f1 == NULL) {
+            printf("[-] %s failed to open. Exit.\n", pathname_state1);
+            exit(1);
+        }
+        size_t ret1 = fread(Data1, 1, Size, f1);
+        assert(ret1 == Size);
+        fclose(f1);
     }
-    size_t ret1 = fread(Data1, 1, Size, f1);
-    assert(ret1 == Size);
-    fclose(f1);
-
     // merge
+    int a = 0, b = 0, c;
     for (int i = 0; i < Size; i++) {
-        Data0[i] += Data1[i];
+	if (Data0[i] | Data1[i])
+            Data0[i] = 1;
+	else
+            Data0[i] = 0;
     }
-    FILE *fo = fopen(pathname_state0, "wb");
+    FILE *fo = fopen(pathname_output, "wb");
     if (fo == NULL) {
-        printf("[-] %s failed to open. Exit.\n", pathname_state0);
+        printf("[-] %s failed to open. Exit.\n", pathname_output);
         exit(1);
     }
     size_t reto = fwrite(Data0, 1, Size, fo);
