fce66f9c5a95399fc4c611f8da72c14ea4701ed1
Qiang Liu <cyruscyliu@gmail.com>
2022-11-27 12:08:32 +0000

    update vbox results

--- covtablegen-new.sh
+++ covtablegen-new.sh
@@ -3,6 +3,7 @@ PATTERN=$2
 
 rm -f /tmp/xxxVirtFuzzxxx.$FILE
 for ROUND in $(seq 0 9); do
+    # 19 func, 21 line
     grep -e $FILE $PATTERN$ROUND* | sort -t- -k12 | tr -s ' ' | awk -F":|-| " '{print $12,$9$11,$NF}' | python3 rebase-timestamp.py >> /tmp/xxxVirtFuzzxxx.$FILE
 done
 cat /tmp/xxxVirtFuzzxxx.$FILE | python3 covtablegen.py
