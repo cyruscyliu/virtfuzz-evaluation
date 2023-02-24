32f6943bafec9a2c52c23ce7491438e3b6b442fa
Qiang Liu <cyruscyliu@gmail.com>
2023-02-24 17:50:48 +0100

    update

---
+++ state-results/replot-ev31.sh
@@ -0,0 +1,6 @@
+# evaluation 3
+python3 ../cov24plot2.py \
+    videzzo-qemu-xhci-arp.states \
+    videzzo++-qemu-xhci-arp.states \
+    nyx-qemu-qemu_xhci-none.states \
+    VPN
---
+++ state-results/replot-ev32.sh
@@ -0,0 +1,6 @@
+# evaluation 3
+python3 ../cov24plot2.py \
+    videzzo-qemu-xhci-arp.transitions \
+    videzzo++-qemu-xhci-arp.transitions \
+    nyx-qemu-qemu_xhci-none.transitions \
+    VPN
